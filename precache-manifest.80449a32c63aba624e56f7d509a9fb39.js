6efad0ad387ead5d8870da3eb4b9e3faaf08a07a
Souvikcse <sayanwinz@gmail.com>
2020-08-02 01:48:32 +0530

    Updates

---
+++ precache-manifest.80449a32c63aba624e56f7d509a9fb39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f668d64a153c31e156f6d4ee69c56e2",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "135c0785f504128267d7",
+    "url": "/Monsters-Rolodex/static/css/main.0e0e2d0f.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Monsters-Rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135c0785f504128267d7",
+    "url": "/Monsters-Rolodex/static/js/main.4417075b.chunk.js"
+  },
+  {
+    "revision": "5dabb937f8f4758d9731",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.fde89bc3.js"
+  }
+]);
